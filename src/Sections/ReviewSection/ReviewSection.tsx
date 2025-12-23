368db48afe5f3dacabe3897a6148cec43fa061ab
Brady Stacey <bradyjstacey@gmail.com>
2025-12-22 21:10:16 -0800

    fixed broken link, added some stuff to be used later

--- src/Sections/ReviewSection/ReviewSection.tsx
+++ src/Sections/ReviewSection/ReviewSection.tsx
@@ -40,12 +40,12 @@ export const ReviewsSection = () => {
 			</div>
 
 			<a
-				href="https://www.google.com/maps/place/nscoelectric"
+				href="https://www.google.com/search?num=10&sca_esv=c18e17d2c1e631b6&rlz=1C5CHFA_enCA1006CA1008&sxsrf=AE3TifOJY9--aNfOE0Q5XxwGf-CWBiCK5g:1766466356177&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-E2wBp6JJkk-1bakHTDpSz98izCQkfAIfedUYp2xOXSJANwdPH00a0JHHxh1vzfLJASIEBzROJG62Xopf04uSoamVzPiC&q=nscoelectric+Reviews&sa=X&ved=2ahUKEwjVq6v999KRAxUvITQIHX9uCYQQ0bkNegQIIhAE&biw=1440&bih=669&dpr=2"
 				target="_blank"
 				rel="noopener noreferrer"
 				className="link-btn"
 			>
-				Read all reviews on Google
+				Read our Google Reviews
 			</a>
 		</section>
 	);
